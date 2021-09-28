022de042594763e31d3c4bec7b3da29a15a8a52c
Leslie Meadows <chiefcog@znzresearch.com>
2021-09-28 13:21:46 -0500

    Initial Commit

---
+++ InfiniteList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteList
+//
+//  Created by Leslie Meadows on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfiniteList/InfiniteListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteListApp.swift
+//  InfiniteList
+//
+//  Created by Leslie Meadows on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
